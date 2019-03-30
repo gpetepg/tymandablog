55ebd28e59670a75d165fbaa64f6b01411ba92fc
Ty <tylereguo@gmail.com>
2019-03-30 14:55:38 -0700

    Added cache

--- blog/extensions.py
+++ blog/extensions.py
@@ -1,5 +1,12 @@
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_caching import Cache
 
 db = SQLAlchemy()
 migrate = Migrate()
+cache = Cache(config={
+    "DEBUG": True,          # some Flask specific configs
+    "CACHE_TYPE": "simple", # Flask-Caching related configs
+    "CACHE_DEFAULT_TIMEOUT": 300
+    }
+)
